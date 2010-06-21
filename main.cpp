44c6fc8e45510e5c783b2765d964f5b8a0c133d8
Luke J. F. Schlather <luke.schlather@gmail.com>
2010-06-21 12:30:10 -0400

    Improved options, made battery of tests optional.

--- main.cpp
+++ main.cpp
@@ -34,14 +34,18 @@ int main(int argc, char** argv) {
   ImageSet::setHeight(18);
   float scale = 1;
 
+
+
   string directory("/home/project");
   bool recurse=0;
   string moldPath("/home/project/Pittsburgh_November_035.JPG");
   string outputDirectory("/home/project/output/");
   string usage("ImageWeave -d <directory> \n -h : Print this message. \n -x <width> : set x dimension for image cells  \n -y <height> : set y dimension for image cells \n");
 
+  bool battery=false; // run a battery of tests
+
   char c;
-  while ((c =getopt(argc,argv,"hd:s:x:y:t:p:m:i:c:o:R"))!=-1) {
+  while ((c =getopt(argc,argv,"hd:s:x:y:t:p:m:i:c:o:Rb"))!=-1) {
     switch(c) {
     case 'h':
       cout << usage << endl;
@@ -49,6 +53,9 @@ int main(int argc, char** argv) {
       break;
     case 'x':
       ImageSet::setWidth(atof(optarg));
+      break;
+    case 'b':
+      battery=true;
       break;
     case 'm':
       moldPath=string(optarg);
@@ -87,60 +94,59 @@ int main(int argc, char** argv) {
       cout << usage << endl;
     }
   } 
-  // if(optind<argc) {
-  // }
-  // else {
-  //   cout << "No image specified" << endl;
-  //   cout << usage << endl;
-  //   exit(0);
-  // } 
-
 
   ImageSet bunch = ImageSet(directory.c_str(),recurse);
 
   cout << "Read in " << bunch.count() << " images." << endl;
-  
-  //  CImgDisplay library(  bunch.weaveAll(10,10), "Image Library");
-  //bunch.sort(threshold,pct);
-  //  CImgDisplay blank(mold);
-
-  // string testset[10] = {
-  //   moldPath,
-  //   "/home/pictures/Facebook/Travis Hartman/28694_10150183527095147_586850146_12291633_1995_n.jpg",
-  //   "/home/pictures/Facebook/Travis Hartman/28533_10150193005605147_586850146_12513724_3583264_n.jpg",
-  //   "/home/pictures/2009/PitStop Lindy Hop/Pittsburgh_November_088.JPG",
-  //   "/home/pictures/2009/PitStop Lindy Hop/Pittsburgh_November_104.JPG",
-  //   "/home/pictures/2009/PitStop Lindy Hop/Pittsburgh_November_070.JPG",
-  //   "/home/pictures/Facebook/Travis Hartman/32533_10150201850665147_586850146_12770126_6931221_n.jpg",
-  //   "/home/pictures/2009/PitStop Lindy Hop/Pittsburgh_November_036.JPG",
-  //   "/home/pictures/Facebook/Travis Hartman/32533_10150201850840147_586850146_12770146_1439247_n.jpg",
-  //   "/home/pictures/Facebook/Travis Hartman/26702_10150210832665147_586850146_13044142_3554681_n.jpg",
-  // };
+  if(battery) {
+    string testset[10] = {
+      moldPath,
+      "/home/pictures/Facebook/Travis Hartman/28694_10150183527095147_586850146_12291633_1995_n.jpg",
+      "/home/pictures/Facebook/Travis Hartman/28533_10150193005605147_586850146_12513724_3583264_n.jpg",
+      "/home/pictures/2009/PitStop Lindy Hop/Pittsburgh_November_088.JPG",
+      "/home/pictures/2009/PitStop Lindy Hop/Pittsburgh_November_104.JPG",
+      "/home/pictures/2009/PitStop Lindy Hop/Pittsburgh_November_070.JPG",
+      "/home/pictures/Facebook/Travis Hartman/32533_10150201850665147_586850146_12770126_6931221_n.jpg",
+      "/home/pictures/2009/PitStop Lindy Hop/Pittsburgh_November_036.JPG",
+      "/home/pictures/Facebook/Travis Hartman/32533_10150201850840147_586850146_12770146_1439247_n.jpg",
+      "/home/pictures/Facebook/Travis Hartman/26702_10150210832665147_586850146_13044142_3554681_n.jpg",
+    };
 
 
-  // for (int i=0;i<10;++i) {
-  //   moldPath=testset[i];
-     CImg<uchar> mold(moldPath.c_str());
-    for (threshold=0;threshold<=100;threshold+=10) {
-      vector< vector<int> > tapestry = bunch.bruteForce(mold,threshold,pct);
-      CImg<uchar> output= bunch.weave(tapestry);
+    for (int i=0;i<10;++i) {
+      moldPath=testset[i];
+      CImg<uchar> mold(moldPath.c_str());
+      for (threshold=0;threshold<=100;threshold+=10) {
+	vector< vector<int> > tapestry = bunch.bruteForce(mold,threshold,pct);
+	CImg<uchar> output= bunch.weave(tapestry);
       
       
-      fs::path outdir(outputDirectory);
-      fs::path moldfile(moldPath);
-      string name = moldfile.filename();
-      stringstream outfilename(outputDirectory);
-      outfilename << printCurrentTime() << "_" << name << "-t" << threshold << "_" << bunch.count() << "_images.ppm";
-      cout << "Writing " << outfilename.str() << endl;
-      output.save((outputDirectory + outfilename.str()).c_str());
+	fs::path outdir(outputDirectory);
+	fs::path moldfile(moldPath);
+	string name = moldfile.filename();
+	stringstream outfilename(outputDirectory);
+	outfilename << printCurrentTime() << "_" << name << "-t" << threshold << "_" << bunch.count() << "_images.ppm";
+	cout << "Writing " << outfilename.str() << endl;
+	output.save((outputDirectory + outfilename.str()).c_str());
+      }
     }
-  // }
 
-  //  CImgDisplay result(output);
-  // while ( !result.is_closed() ) {
-  //   result.wait();
+  } else {
+    CImg<uchar> mold(moldPath.c_str());
+    vector< vector<int> > tapestry = bunch.bruteForce(mold,threshold,pct);
+    CImg<uchar> output= bunch.weave(tapestry);
     
-  // }
+    
+    fs::path outdir(outputDirectory);
+    fs::path moldfile(moldPath);
+    string name = moldfile.filename();
+    stringstream outfilename(outputDirectory);
+    outfilename << printCurrentTime() << "_" << name << "-t" << threshold << "_" << bunch.count() << "_images.ppm";
+    cout << "Writing " << outfilename.str() << endl;
+    output.save((outputDirectory + outfilename.str()).c_str());
+  }
+
+  
   return 0;
 }
 
