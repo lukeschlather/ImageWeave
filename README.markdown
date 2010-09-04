4c0d5666d73fe8ea9201a63c038b680eadffc07c
Luke J. F. Schlather <luke.schlather@gmail.com>
2010-09-03 22:25:21 -0400

    Added support for multiple target images.
    Reading in and resizing images to have a good library has a lot of overhead, so it's a lot faster to be able to run the algorithm at different thresholds with different target images using the same source library of images. I've amended the command line options and main.cpp to make that easy.

--- README.markdown
+++ README.markdown
@@ -25,7 +25,7 @@ should get you what you need as far as boost.
 
 Should get you the version of  CImg I'm using, though I pulled that copy as of 2010-04-08, so no guarantees after that date. :) 
 
-If you have those things, it should be a simple matter of typing `make`. CImg is a template library.
+If you have those things, it should be a simple matter of typing `make`. CImg is a template library, so compiling takes a little while.
 
 ##Use
 
@@ -35,20 +35,22 @@ At the moment, a typical usage might look like this:
 Breaking these options down:
 
     -m /home/project/testS.png
-This is the target "mold" image.
+This is the target "mold" image. You may use this flag multiple times to specify multiple targets. 
     -d /home/project/colors/
-This is the directory where the program reads images. It does not recurse, and though it should simply throw an error if you give it something that isn't an image, behavior is undefined if non-image files exist in this directory.
+This is the directory where the program reads images. CImg should throw an error but ignore non-image files. 
     -R
-Search the target directory recursively. 
+Search the target directory recursively. Without this flag it uses only images in the root directory. 
+    -b 
+Instead of using a single threshold, run a battery of 10 tests with i/threshold as the threshold. I use it with `-t 100` to see the results at thresholds 10,20 ... 100 .
      -t50
 This is the threshold for deciding if a color matches. Color is stored in 24 bits to define the color for a pixel, that is, three 8-bit unsigned chars which define the red, green, and blue color channels. The value should be from 0-255, otherwise it will overflow. Currently this doesn't actually do anything; the program runs a battery everywhere from 0-100 stepping by 10 so you can compare. 
 
+Before the algorithm is run, ImageWeave resizes all of the source images so that they can easily be stored in memory and efficiently compared. By default, it is set to 24x18, which assumes that your mold and source images are all at a 4:3 aspect ratio, because that's what my camera has. Note that this will likely stretch images, or show them sideways. At a minimum I would suggest ensuring that the x dimension of your mold is evenly divisible by the x you choose for cell width, and the same with y and height. 
 Cell Width and height can be set as follows:
      -x20
      -y10
-They default to 24x18
 
-It will drop a copy of the finished image in the directory set by the flag -o:
+It will drop a copy of the finished image[s] in the directory set by the flag -o:
      -o /home/project/output/
 /home/project/output/ is the default directory.
 
--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ void strike(CImg<uchar> & img,int x, int y, int c, int len) {
 
 int main(int argc, char** argv) {
   
-  int threshold = 40;
+  int threshold;
   double pct = .75;
   double iterations =1000;
   double popcount = 480;
@@ -38,7 +38,7 @@ int main(int argc, char** argv) {
 
   string directory("/home/project");
   bool recurse=0;
-  string moldPath("/home/project/Pittsburgh_November_035.JPG");
+  vector<string> moldPaths;
   string outputDirectory("/home/project/output/");
   string usage("ImageWeave -d <directory> \n -h : Print this message. \n -x <width> : set x dimension for image cells  \n -y <height> : set y dimension for image cells \n");
 
@@ -58,7 +58,7 @@ int main(int argc, char** argv) {
       battery=true;
       break;
     case 'm':
-      moldPath=string(optarg);
+      moldPaths.push_back(string(optarg));
       break;
     case 'd':
       directory = string(optarg);
@@ -98,53 +98,36 @@ int main(int argc, char** argv) {
   ImageSet bunch = ImageSet(directory.c_str(),recurse);
 
   cout << "Read in " << bunch.count() << " images." << endl;
-  if(battery) {
-    string testset[10] = {
-      moldPath,
-      "/home/pictures/Facebook/Travis Hartman/28694_10150183527095147_586850146_12291633_1995_n.jpg",
-      "/home/pictures/Facebook/Travis Hartman/28533_10150193005605147_586850146_12513724_3583264_n.jpg",
-      "/home/pictures/2009/PitStop Lindy Hop/Pittsburgh_November_088.JPG",
-      "/home/pictures/2009/PitStop Lindy Hop/Pittsburgh_November_104.JPG",
-      "/home/pictures/2009/PitStop Lindy Hop/Pittsburgh_November_070.JPG",
-      "/home/pictures/Facebook/Travis Hartman/32533_10150201850665147_586850146_12770126_6931221_n.jpg",
-      "/home/pictures/2009/PitStop Lindy Hop/Pittsburgh_November_036.JPG",
-      "/home/pictures/Facebook/Travis Hartman/32533_10150201850840147_586850146_12770146_1439247_n.jpg",
-      "/home/pictures/Facebook/Travis Hartman/26702_10150210832665147_586850146_13044142_3554681_n.jpg",
-    };
-
-
-    for (int i=0;i<10;++i) {
-      moldPath=testset[i];
-      CImg<uchar> mold(moldPath.c_str());
-      for (threshold=0;threshold<=100;threshold+=10) {
-	vector< vector<int> > tapestry = bunch.bruteForce(mold,threshold,pct);
-	CImg<uchar> output= bunch.weave(tapestry);
-      
-      
-	fs::path outdir(outputDirectory);
-	fs::path moldfile(moldPath);
-	string name = moldfile.filename();
-	stringstream outfilename(outputDirectory);
-	outfilename << printCurrentTime() << "_" << name << "-t" << threshold << "_" << bunch.count() << "_images.ppm";
-	cout << "Writing " << outfilename.str() << endl;
-	output.save((outputDirectory + outfilename.str()).c_str());
-      }
-    }
-
-  } else {
+  for (unsigned int i=0;i<moldPaths.size();++i) {
+    string moldPath=moldPaths[i];
     CImg<uchar> mold(moldPath.c_str());
-    vector< vector<int> > tapestry = bunch.bruteForce(mold,threshold,pct);
-    CImg<uchar> output= bunch.weave(tapestry);
+    int maxThreshold;
+    int step=10;
+    if(battery) {
+      maxThreshold=threshold;
+      threshold=threshold/10;
+      step=threshold;
+    }
+    else { 
+      maxThreshold=threshold; 
+      step=100;
+    }
+    for (;threshold<=maxThreshold;threshold+=step) {
+      cout << "step: " << step << endl;
+      CImg<uchar> mold(moldPath.c_str());
+      vector< vector<int> > tapestry = bunch.bruteForce(mold,threshold,pct);
+      CImg<uchar> output= bunch.weave(tapestry);
     
-    
-    fs::path outdir(outputDirectory);
-    fs::path moldfile(moldPath);
-    string name = moldfile.filename();
-    stringstream outfilename(outputDirectory);
-    outfilename << printCurrentTime() << "_" << name << "-t" << threshold << "_" << bunch.count() << "_images.ppm";
-    cout << "Writing " << outfilename.str() << endl;
-    output.save((outputDirectory + outfilename.str()).c_str());
-  }
+	
+      fs::path outdir(outputDirectory);
+      fs::path moldfile(moldPath);
+      string name = moldfile.filename();
+      stringstream outfilename(outputDirectory);
+      outfilename << printCurrentTime() << "_" << name << "-t" << threshold << "_" << bunch.count() << "_images.ppm";
+      cout << "Writing " << outfilename.str() << endl;
+      output.save((outputDirectory + outfilename.str()).c_str());
+    }
+  } 
 
   
   return 0;
